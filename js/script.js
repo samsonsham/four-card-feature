4b221de426f1914a9dd5c53163286549ecde6dcc
Samson Sham <samsonsham@gmail.com>
2022-03-17 23:27:13 +0000

    improve html semantic

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const cards = [
     title: 'Supervisor',
     content: 'Monitors activity to identify project roadblocks',
     image: 'icon-supervisor.svg',
-    alt: 'magnifier',
   },
   {
     id: 2,
@@ -12,14 +11,12 @@ const cards = [
     content:
       'Scans our talent network to create the optimal team for your project',
     image: 'icon-team-builder.svg',
-    alt: 'a house in front of a webpage',
   },
   {
     id: 3,
     title: 'Karma',
     content: 'Regularly evaluates our talent to ensure quality',
     image: 'icon-karma.svg',
-    alt: 'light bulb',
   },
   {
     id: 4,
@@ -27,6 +24,5 @@ const cards = [
     content:
       'Uses data from past projects to provide better delivery estimates',
     image: 'icon-calculator.svg',
-    alt: 'data on screen',
   },
 ];
